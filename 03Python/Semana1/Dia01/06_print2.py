149a15b1ab1413907225029e48e3729008f811ca
blandskron <bastianlandskronfreelancer@gmail.com>
2024-01-22 00:06:58 -0300

    modulopython

---
+++ 03Python/Semana1/Dia01/06_print2.py
@@ -0,0 +1,21 @@
+print("Hola, esto es el archivo 06")
+
+print("Hola soy Nombre y tengo 12")
+
+# operaciones...
+
+print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+# Esto es un comentario
+"""
+varias
+lineas
+otra
+"""
+'''
+varias 
+lienas
+'''
---
+++ 03Python/Semana1/Dia02/00_types.py
@@ -0,0 +1,20 @@
+# string
+my_name = "Nicolas"
+my_name = 'Santiago'
+print('my_name =>', my_name)
+print(type(my_name))
+
+# int
+my_age = 12
+print('my_age =>', my_age)
+print(type(my_age))
+
+# boolean
+is_single = False
+print('is_single =>', is_single)
+print(type(is_single))
+
+# inputs
+my_age = input('Cual es tu edad? ')
+print('my_age =>', my_age)
+print(type(my_age))
