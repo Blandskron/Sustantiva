d3e8953d256f9b713a4f25ac207e332ecd3807c5
blandskron <bastianlandskronfreelancer@gmail.com>
2024-02-11 15:07:18 -0300

    dia07

---
+++ 03Python/Semana2/Dia07/10_repasoPrint.py
@@ -0,0 +1,16 @@
+# Imprime un saludo simple
+print("¡Hola mundo!")
+
+# Define una variable 'edad' y la imprime
+edad = 32
+print(edad)
+
+# Define variables 'nombre' y 'edad', e imprime un mensaje que las combina
+nombre = "Ana"
+print("Mi nombre es", nombre, "y tengo", edad, "años de edad.")
+
+# Utiliza el método .format() para insertar valores en una cadena
+print("Mi nombre es {} y tengo {} años de edad.".format(nombre, edad))
+
+# Utiliza f-strings para insertar valores directamente en la cadena (Python 3.6+)
+print(f"Mi nombre es {nombre} y tengo {edad} años de edad.")
--- 03Python/Semana2/Dia07/ejercicio02.py
+++ 03Python/Semana2/Dia07/ejercicio02.py
@@ -1,4 +1,7 @@
+# Bucle externo que recorre los números del 1 al 10
 for a in range(1, 11):
     
-    for b in range(1, 1):
+    # Bucle interno que recorre los números del 1 al 10 para multiplicar con 'a'
+    for b in range(1, 11):
+        # Imprime la multiplicación de 'a' y 'b' junto con un mensaje
         print(a, "*", b, "=", a*b)
