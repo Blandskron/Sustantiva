8270f341850c061341fadf0a0f7ffb9bbcfcb918
blandskron <bastianlandskronfreelancer@gmail.com>
2024-02-11 19:51:59 -0300

    dia 10

---
+++ 03Python/Semana2/Dia10/ejercicio1.py
@@ -0,0 +1,13 @@
+# Creamos una lista llamada list1 con algunos elementos.
+list1 = [0, 1, 2, 3, 4]
+
+# Imprimimos los elementos de la lista y su longitud original.
+print("los elementos de la lista son: ", list1)
+print("el largo de la lista original es: ", len(list1))
+
+# Eliminamos el elemento en la posición 1 de la lista.
+del list1[1]
+
+# Imprimimos los elementos de la lista modificada y su longitud.
+print("\n")
+print("los elementos de la lista modificada son: ", list1, "y su largo es: ", len(list1))
