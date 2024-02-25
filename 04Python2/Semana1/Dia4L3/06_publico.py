bd3977884cba0da53a76a6093f126479e830a034
blandskron <bastianlandskronfreelancer@gmail.com>
2024-02-25 20:51:16 -0300

    update

--- 04Python2/Semana1/Dia4L3/06_publico.py
+++ 04Python2/Semana1/Dia4L3/06_publico.py
@@ -1,9 +1,12 @@
+# Definición de la clase EjemploPublico
 class EjemploPublico:
+    # Método constructor
     def __init__(self):
+        # Inicialización del atributo público
         self.publico = "Este es un atributo público"
 
-# Creación de instancia de la clase
+# Creación de una instancia de la clase EjemploPublico
 objeto_publico = EjemploPublico()
 
-# Acceso al atributo público
+# Acceso al atributo público e impresión de su valor
 print(objeto_publico.publico)
--- 04Python2/Semana1/Dia5L3/07_tiposAnimales.py
+++ 04Python2/Semana1/Dia5L3/07_tiposAnimales.py
@@ -1,11 +1,13 @@
 class TiposAnimales:
     def __init__(self, tipo):
-        self.tipo = tipo
+        self.tipo = tipo  # Define el tipo de animales
 
     def clasificacion(self):
+        # Método para imprimir la clasificación de los animales según su tipo
         print(f"Los animales de tipo {self.tipo} se clasifican según diferentes criterios.")
 
-
-# Uso de la clase
+# Creación de una instancia de la clase TiposAnimales para mamíferos
 mamiferos = TiposAnimales("mamíferos")
+
+# Llamada al método clasificacion() para imprimir la clasificación de los mamíferos
 mamiferos.clasificacion()
